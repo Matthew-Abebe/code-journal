f0a9275e028731a880f787ba2ab67e8595e3d12d
Matthew Abebe <matthewabebe.inbox@gmail.com>
2021-03-30 15:05:08 -0700

    Styling changes.

--- js/main.js
+++ js/main.js
@@ -1,2 +1,47 @@
 /* global data */
 /* exported data */
+
+var $photoUrl = document.querySelector('#photo_url');
+var $photo = document.querySelector('#photo');
+
+$photoUrl.addEventListener('input', updatePhotoUrl);
+
+function updatePhotoUrl(event) {
+  $photo.setAttribute('src', event.target.value);
+}
+
+var $formElement = document.querySelector('.form-element');
+
+$formElement.addEventListener('submit', handleSubmitForm);
+
+function handleSubmitForm(event) {
+  event.preventDefault();
+
+  var inputObj = {
+    title: $formElement.elements['title-name'].value,
+    photoURL: $formElement.elements['photo-url'].value,
+    notes: $formElement.elements['notes-section'].value,
+    entryId: data.nextEntryId++
+  };
+
+  var newEntry = data.entries.unshift(inputObj);
+
+  $formElement.reset();
+
+  $photo.setAttribute('src', 'images/placeholder-image-square.jpg');
+
+  return newEntry;
+}
+
+// var codeJournalEntries = localStorage.getItem('code-journal-data');
+
+// if (previousEntriesJSON !== null) {
+//   data.entries = JSON.parse(previousEntriesJSON);
+// }
+window.addEventListener('beforeunload', handleBeforeUnload);
+
+function handleBeforeUnload(event) {
+
+  var dataJSON = JSON.stringify(data);
+  localStorage.setItem('code-journal-data', dataJSON);
+}
